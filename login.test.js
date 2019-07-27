97c21bc8d28ec89dc9a8630b37ed50600fd2e1e9
jkcon <github@xiixii.pw>
2019-07-27 10:35:46 -0500

    Add invalid login tests

---
+++ login.test.js
@@ -0,0 +1,41 @@
+describe('Login page', () => {
+  jest.setTimeout(10000) // set global timeout based on network/browser
+  beforeAll(async () => {
+    await page.setViewport({width: 1200, height: 900})
+    await page.goto('https://login.thrivehive.com/login')
+  })
+
+  it('should display login page', async (done) => {
+    await page.waitFor('button[type="submit"]')
+    await page.screenshot({ path:'./screenshots/blank-login-page.png'})
+    await expect(page).toMatch('Sign In')
+    done()
+  })
+
+  it('should prompt for email when given invalid format input', async (done) => {
+    await expect(page).toFillForm('form[action="/auth/user"]', {
+      username: 'James',
+      password: 'Bond',
+    })
+    await page.screenshot({ path:'./screenshots/filled-login-page.png'})
+    await expect(page).toClick('button', 'type="submit"')
+    await page.waitFor(1000)
+    await expect(page).toMatch('Enter your email to login')
+    await page.screenshot({ path:'./screenshots/invalid-email-login-page.png'})
+    done()
+  })
+
+  it('shoudld prompt invalid login credentials', async (done) => {
+    await expect(page).toFillForm('form[action="/auth/user"]', {
+      username: 'James@bond.com',
+      password: 'Bond',
+    })
+    await page.screenshot({ path:'./screenshots/revised-login-page.png'})
+    await expect(page).toClick('button', 'type="submit"')
+    await page.waitFor(1000)
+    await expect(page).toMatch('Invalid user credentials')
+    await page.screenshot({ path:'./screenshots/invalid-credentials-login-page.png'})
+    done()
+  })
+
+})
